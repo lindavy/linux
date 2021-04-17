f5b767aba2cde5ebc33c4fce7da7530960ef8a9c
Linda Nguyen <lindavy96@gmail.com>
2021-04-17 13:40:45 -0700

    added exit and procbased controls

--- assignment1/cmpe283-1.c
+++ assignment1/cmpe283-1.c
@@ -13,6 +13,8 @@
  */
 #define IA32_VMX_PINBASED_CTLS	0x481
 #define IA32_VMX_ENTRY_CTLS	0x484
+#define IA32_VMX_EXIT_CTLS      0x483	
+#define IA32_VMX_PROCBASED_CTLS 	0x482
 #define IA32_VMX_PROCBASED_CTLS2	0x48B
 
 
@@ -56,6 +58,53 @@ struct capability_info entry_controls[12] =
   { 18, "Load IA32_RTIT_CTL"},
   { 20, "Load CET state"},
   { 22, "Load PKRS"}
+};
+
+
+// Exit Controls
+struct capability_info exit_controls[14] = 
+{
+	{ 2, "Save debug controls" }, 
+	{ 9, "Host address-space size" }, 
+	{ 12, "Load IA32_PERF_GLOBAL_CTRL" }, 
+	{ 15, "Acknowledge interrupt on exit" }, 
+	{ 18, "Save IA32_PAT" }, 
+	{ 19, "Load IA32_PAT" }, 
+	{ 20, "Save IA32_EFER" }, 
+	{ 21, "Load IA32_EFER" }, 
+	{ 22, "Save VMX-preemption timer value" }, 
+	{ 23, "Clear IA32_BNDCFGS" }, 
+	{ 24, "Conceal VMX from PT" }, 
+	{ 25, "Clear IA32_RTIT_CTL" }, 
+	{ 28, "Load CET state" }, 
+	{ 29, "Load PKRS" }
+}; 
+
+// Procbased Controls
+struct capability_info procbased_controls[22] = 
+{
+	{ 2, "Interrupt-window exiting" }, 
+	{ 3, "Use TSC Offsetting" }, 
+	{ 7, "HLT exiting" }, 
+	{ 9, "INVLPG exiting" }, 
+	{ 10, "MWAIT exiting" }, 
+	{ 11, "RDPMC exiting" }, 
+	{ 12, "RDTSC exiting" },
+	{ 15, "CR3-load exiting" }, 
+	{ 16, "CR3-store exiting" }, 
+	{ 17, "Activate tertiary controls" }, 
+	{ 19, "CR8-load exiting" }, 
+	{ 20, "CR8-store exiting" }, 
+	{ 21, "Use TPR shadow" }, 
+	{ 22, "NMI-window exiting" }, 
+	{ 23, "MOV-DR exiting" }, 
+	{ 24, "Unconditional IO exiting" }, 
+	{ 25, "Use IO bitmaps" }, 
+	{ 27, "Monitor trap flag" }, 
+	{ 28, "Use MSR bitmaps" }, 
+	{ 29, "MONITOR exiting" }, 
+	{ 30, "PAUSE exiting" }, 
+	{ 31, "Activate secondary controls" } 
 };
 
 //Secondary Proc Based
@@ -143,6 +192,18 @@ detect_vmx_features(void)
 	pr_info("Entry Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(entry_controls, 12, lo, hi);
+
+  /* Exit controls */
+	rdmsr(IA32_VMX_EXIT_CTLS, lo, hi);
+	pr_info("Exit Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(exit_controls, 14, lo, hi);
+
+  /* Proc Based controls */
+	rdmsr(IA32_VMX_PROCBASED_CTLS, lo, hi);
+	pr_info("Procbased Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(procbased_controls, 22, lo, hi);
 
   /* Secondary Proc Based controls */
 	rdmsr(IA32_VMX_PROCBASED_CTLS2, lo, hi);
